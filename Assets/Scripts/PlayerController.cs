c2ff2ce8d9c46b6724e383776220f3a4804a5f04
wpc3 <williamchapman53@gmail.com>
2024-12-10 16:08:56 -0500

    added movement vector and rigidbody to script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,13 +3,18 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private Rigidbody rb;
     void Start()
     {
-        
+        rb = GetComponet<Rigidbody>();
     }
 void OnMove(InputValue movementValue){
     //Function body
-
+    Vector2 movementVector = movementValue.Get<Vector2>();
+}
+void FixedUpdate(){
+    
 }
     
 }
