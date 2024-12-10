1d21c9c770534cebc6a266ee95e32d03f55ee8c4
wpc3 <williamchapman53@gmail.com>
2024-12-10 15:30:56 -0500

    adding script to move the ball

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+void OnMove(InputValue movementValue){
+    //Function body
+
+}
+    
+}
