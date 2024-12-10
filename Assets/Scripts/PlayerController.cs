fb0f0184f98eb2a43d84e1e9952dcf93520c394a
wpc3 <williamchapman53@gmail.com>
2024-12-10 16:44:25 -0500

    got the ball moving!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,16 +5,23 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     private Rigidbody rb;
+    private float movementX;
+    private float movementY;
     void Start()
     {
-        rb = GetComponet<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
     }
 void OnMove(InputValue movementValue){
     //Function body
     Vector2 movementVector = movementValue.Get<Vector2>();
+
+    movementX = movementVector.x;
+    movementY = movementVector.y;
 }
 void FixedUpdate(){
-    
+    Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+    rb.AddForce(movement);
 }
     
 }
