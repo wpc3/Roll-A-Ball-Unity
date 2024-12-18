22cd8c126370932ee94b6260f200846fb870533a
wpc3 <williamchapman53@gmail.com>
2024-12-18 14:34:22 -0500

    added comments to script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,25 +4,34 @@ public class PlayerController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
+    //Speed of the player
     public float speed = 0;
-
+    //Rigid body of the playeer
     private Rigidbody rb;
+    //Movement along x and y axis
     private float movementX;
     private float movementY;
+
+    //Start is called before the first frame update    
     void Start()
-    {
+    {   //Get and store the Rigidbody component attached to the player
         rb = GetComponent<Rigidbody>();
     }
+//Function is called when a move input is detected
 void OnMove(InputValue movementValue){
-    //Function body
+    //Convert the input value into a Vector2 for movement
     Vector2 movementVector = movementValue.Get<Vector2>();
 
+    //Store the x and y components of the movement
     movementX = movementVector.x;
     movementY = movementVector.y;
 }
+//Fixed Update is called once per fixed frame-rate frame
 void FixedUpdate(){
+    //Create a 3D movement vecture using the x and y inputs
     Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
+    //Add force to the Rigidbody to move the player
     rb.AddForce(movement * speed);
 }
     
