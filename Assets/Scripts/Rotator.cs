d4fe37f6a2f6cc728f7203285ad40b6159de7434
wpc3 <williamchapman53@gmail.com>
2024-12-19 14:21:20 -0500

    added comments to script

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -8,6 +8,7 @@ public class Rotator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Rotate the object on X, Y, and Z axes by specified amounts, adjusted for frame rate.
        transform.Rotate(new Vector3(15,30,45) * Time.deltaTime); 
     }
 }
